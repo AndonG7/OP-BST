20a9174a5d0b1e146ef2f3eb6f34b47c8be44735
AndonG7 <gjorgjiev.andon@uklo.edu.mk>
2023-03-16 19:39:12 +0100

    new functions

--- rebalance.js
+++ rebalance.js
@@ -1,24 +1,28 @@
 import { buildTree } from "./buildTree.js";
 import { Tree } from "./Tree.js";
 
+//Declaring new Array
 let newArr = [];
 
+//Filling that array in increasing order
 function inOrder(_root) {
     _root.left && inOrder(_root.left);
     newArr.push(_root.data);
     _root.right && inOrder(_root.right);
 } 
 
-
-
+//Rebalancing function
 export function rebalance(_root, _balanced) {
+    //If the tree is already balanced, exit
     if (_balanced) {
         console.log("Tree is balanced!");
         return;
     }
 
+    //Calling the function that fills the array
     inOrder(_root);
 
+    //Creating new tree that is balanced
     let t2 = new Tree(newArr);
 
     return t2.root;
